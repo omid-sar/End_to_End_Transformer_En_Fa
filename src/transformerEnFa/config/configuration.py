87ec443f24ca9ef65b754f74393b72f661eb6b87
omid-sar <mr.omid.sardari@gmail.com>
2024-02-05 13:04:21 -0500

    Data validation stage completed

--- src/transformerEnFa/config/configuration.py
+++ src/transformerEnFa/config/configuration.py
@@ -1,6 +1,7 @@
 from transformerEnFa.constants import *
 from transformerEnFa.utils.common import read_yaml, create_directories
 from transformerEnFa.entity import DataIngestionConfig
+from transformerEnFa.entity import DataValidationConfig
 
 
 class ConfigurationManager:
@@ -28,3 +29,16 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    def get_data_validation_config(self)-> DataValidationConfig:
+        config = self.config.data_validation
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+        root_dir = config.root_dir,
+        STATUS_FILE = config.STATUS_FILE,
+        ALL_REQUIRED_FILES= config.ALL_REQUIRED_FILES
+        )
+
+        return data_validation_config

82ca9d38eef7fdc164fef392e2c57b0149d10879
omid-sar <mr.omid.sardari@gmail.com>
2024-02-16 09:47:27 -0500

    change logger for data ingestion stage

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from transformerEnFa.pipeline.stage_06_model_evaluation import ModelEvaluationPi
 
 STAGE_NAME = "Data Ingestion stage"
 try:
-   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   logger.info(f"\n\nx{'=' * 80}x \n\n>>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx{'=' * 80}x")

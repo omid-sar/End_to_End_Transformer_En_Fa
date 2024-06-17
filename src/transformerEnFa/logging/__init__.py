cb8ef74a9e6b4078f3c1fcc4fb8e61e56c48b463
omid-sar <mr.omid.sardari@gmail.com>
2024-06-17 10:43:29 -0400

    modifying path

--- src/transformerEnFa/logging/__init__.py
+++ src/transformerEnFa/logging/__init__.py
@@ -3,9 +3,14 @@ import sys
 import logging
 
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
-log_dir = "logs"
+
+absolute_path = os.path.abspath(os.path.dirname(__file__))
+base_dir = os.path.abspath(os.path.join(absolute_path, "../../.."))
+log_dir = os.path.join(base_dir, "logs")
 log_filepath = os.path.join(log_dir,"running_logs.log")
+
 os.makedirs(log_dir, exist_ok=True)
+
 
 
 
